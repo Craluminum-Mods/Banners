c6573c27142558cbf4a151396f1c86ecd29f4f73
Craluminum2413 <beware.14b@gmail.com>
2024-05-27 19:07:52 +0300

    separate commands into client and server

--- src/Systems/Commands/Client.cs
+++ src/Systems/Commands/Client.cs
@@ -3,9 +3,9 @@ using Vintagestory.API.Common;
 using Vintagestory.API.Common.CommandAbbr;
 using Vintagestory.API.Util;
 
-namespace Flags;
+namespace Flags.Commands;
 
-public class Commands : ModSystem
+public class Client : ModSystem
 {
     public override bool ShouldLoad(EnumAppSide forSide) => forSide == EnumAppSide.Client;
 
@@ -16,10 +16,10 @@ public class Commands : ModSystem
 
         commands.BeginSub("gentextures")
             .WithAlias("gentex")
-            .WithDesc(Constants.Commands.GenerateTextures.Description.Localize())
+            .WithDesc(Constants.Commands.GenerateTextures.Description.Localize(OutputFolder))
             .WithArgs(
             parsers.Bool(Constants.Commands.GenerateTextures.OptionalArgReplaceExisting.Localize()),
-            parsers.Word(Constants.Commands.GenerateTextures.OptionalArgGrayscaleColor.Localize()))
+            parsers.Word(Constants.Commands.GenerateTextures.OptionalArgTextureColor.Localize()))
             .HandleWith(GenerateTextures)
         .EndSub();
     }
@@ -27,14 +27,14 @@ public class Commands : ModSystem
     public static TextCommandResult GenerateTextures(TextCommandCallingArgs args)
     {
         bool replaceExisting = args?[0].ToString().ToBool() ?? false;
-        string grayscaleColor = args.LastArg.ToString();
+        string textureColor = args.LastArg.ToString();
         ItemSlot slot = args.Caller.Player.Entity.RightHandItemSlot;
         ICoreClientAPI capi = args.Caller.Player.Entity.Api as ICoreClientAPI;
         if (slot?.Itemstack?.Collectible is not BlockBanner blockBanner)
         {
-            return TextCommandResult.Error(Constants.Commands.GenerateTextures.ErrorNoBanner.Localize());
+            return TextCommandResult.Error(Constants.Commands.ErrorNoBanner.Localize());
         }
-        blockBanner.DebugPregenerateTextures(capi, replaceExisting, grayscaleColor);
+        blockBanner.DebugPregenerateTextures(capi, replaceExisting, textureColor);
         return TextCommandResult.Success(Constants.Commands.GenerateTextures.Success.Localize());
     }
 }
