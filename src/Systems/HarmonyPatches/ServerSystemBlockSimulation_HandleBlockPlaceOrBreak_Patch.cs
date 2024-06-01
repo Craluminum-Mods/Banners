039cdd0aa519f378a35d6b2eb6b46c20ccc303fb
Craluminum2413 <beware.14b@gmail.com>
2024-06-01 17:17:18 +0300

    remove try catch

--- src/Systems/HarmonyPatches/ServerSystemBlockSimulation_HandleBlockPlaceOrBreak_Patch.cs
+++ src/Systems/HarmonyPatches/ServerSystemBlockSimulation_HandleBlockPlaceOrBreak_Patch.cs
@@ -12,29 +12,21 @@ public static class ServerSystemBlockSimulation_HandleBlockPlaceOrBreak_Patch
 {
     public static void Prefix(ServerSystemBlockSimulation __instance, Packet_Client packet, ConnectedClient client)
     {
-        try
+        Packet_ClientBlockPlaceOrBreak p = packet.BlockPlaceOrBreak;
+        BlockSelection blockSel = new BlockSelection
         {
-            Packet_ClientBlockPlaceOrBreak p = packet.BlockPlaceOrBreak;
-            BlockSelection blockSel = new BlockSelection
-            {
-                DidOffset = p.DidOffset > 0,
-                Face = BlockFacing.ALLFACES[p.OnBlockFace],
-                Position = new BlockPos(p.X, p.Y, p.Z),
-                HitPosition = new Vec3d(CollectibleNet.DeserializeDouble(p.HitX), CollectibleNet.DeserializeDouble(p.HitY), CollectibleNet.DeserializeDouble(p.HitZ)),
-                SelectionBoxIndex = p.SelectionBoxIndex
-            };
+            DidOffset = p.DidOffset > 0,
+            Face = BlockFacing.ALLFACES[p.OnBlockFace],
+            Position = new BlockPos(p.X, p.Y, p.Z),
+            HitPosition = new Vec3d(CollectibleNet.DeserializeDouble(p.HitX), CollectibleNet.DeserializeDouble(p.HitY), CollectibleNet.DeserializeDouble(p.HitZ)),
+            SelectionBoxIndex = p.SelectionBoxIndex
+        };
 
-            if (blockSel != null
-                && !blockSel.IsProtected(client.Player.Entity.World, client.Player, EnumBlockAccessFlags.BuildOrBreak)
-                && client.Player.Entity.World.BlockAccessor.TryGetBEBehavior(blockSel, out BEBehaviorBannerContainable bebehavior))
-            {
-                bebehavior.TryTake(blockSel);
-            }
-        }
-        catch (System.Exception e)
+        if (blockSel != null
+            && !blockSel.IsProtected(client.Player.Entity.World, client.Player, EnumBlockAccessFlags.BuildOrBreak)
+            && client.Player.Entity.World.BlockAccessor.TryGetBEBehavior(blockSel, out BEBehaviorBannerContainable bebehavior))
         {
-            client?.Player?.Entity?.World?.Logger.Error("[Flags] Not able to get banner from BannerContainable block");
-            client?.Player?.Entity?.World?.Logger.Error(e);
+            bebehavior.TryTake(blockSel);
         }
     }
 }
