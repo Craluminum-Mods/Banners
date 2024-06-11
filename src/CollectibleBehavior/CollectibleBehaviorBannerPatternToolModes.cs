23ca038e159a3b5aa825ca057ed2413ea77d3ae1
Craluminum2413 <beware.14b@gmail.com>
2024-06-11 12:54:49 +0300

    add tool modes to separate namespace

--- src/CollectibleBehavior/CollectibleBehaviorBannerPatternToolModes.cs
+++ src/CollectibleBehavior/CollectibleBehaviorBannerPatternToolModes.cs
@@ -1,3 +1,4 @@
+using Flags.ToolModes;
 using System;
 using System.Collections.Generic;
 using System.Linq;
--- src/Utility/PatternToolMode.cs
+++ src/Utility/PatternToolMode.cs
@@ -4,13 +4,13 @@ using Vintagestory.API.Client;
 using Vintagestory.API.Common;
 using Vintagestory.API.Util;
 
-namespace Flags;
+namespace Flags.ToolModes;
 
 public class PatternToolMode
 {
     public string Pattern { get; set; }
     public bool Linebreak { get; set; }
-    public UnlockableBy[] Unlockable { get; set; } = null;
+    public Unlockable[] Unlockable { get; set; } = null;
 
     public SkillItem GetToolMode(ICoreClientAPI capi, ItemSlot slot)
     {
@@ -98,19 +98,19 @@ public class PatternToolMode
     {
         return Unlockable == null || (Unlockable != null && BannerPatternProperties.FromStack(slot.Itemstack).IsUnlocked(Pattern));
     }
+}
 
-    public class UnlockableBy
+public class Unlockable
+{
+    public string Code { get; set; }
+    public EnumItemClass Type { get; set; }
+    public Dictionary<string, string> HasAttributes { get; set; } = new();
+
+    public bool Matches(ItemStack stack)
     {
-        public string Code { get; set; }
-        public EnumItemClass Type { get; set; }
-        public Dictionary<string, string> HasAttributes { get; set; } = new();
-
-        public bool Matches(ItemStack stack)
-        {
-            return stack != null && !string.IsNullOrEmpty(Code)
-                && new AssetLocation(Code) == stack.Collectible.Code
-                && Type == stack.Class
-                && (HasAttributes == null || (HasAttributes != null && HasAttributes.All(x => stack.Attributes.GetAsString(x.Key) == x.Value)));
-        }
+        return stack != null && !string.IsNullOrEmpty(Code)
+            && new AssetLocation(Code) == stack.Collectible.Code
+            && Type == stack.Class
+            && (HasAttributes == null || (HasAttributes != null && HasAttributes.All(x => stack.Attributes.GetAsString(x.Key) == x.Value)));
     }
 }
